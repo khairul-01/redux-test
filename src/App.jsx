8628659493ced8d0613de9f601a58b1b0b0c9f2c
Khairul Alam <khairul431743@gmail.com>
2024-02-25 17:19:58 +0600

    created redux project and first comment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-purple-400 text-3xl text-center">Hello Redux</h1>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="bg-blue-400 text-2xl">This is Home page</h1>
+        </div>
+    );
+};
+
+export default Home;
